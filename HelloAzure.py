a2e8eb546046dd4247ebbd7006e78b18080d2b07
Aaron Zhang <AaronZhangAus@users.noreply.github.com>
2021-01-15 12:29:25 +1030

    add files

--- HelloAzure.py
+++ HelloAzure.py
@@ -1,5 +1,14 @@
-from azureml.core import VERSION,Workspace
+from azureml.core import Workspace, Experiment, ScriptRunConfig
+from azureml.widgets import RunDetails
+
 
 #connect to workspace from config.json file
 ws=Workspace.from_config("config.json")
-print(VERSION)
+
+experiment = Experiment(workspace=ws, name='day1-experiment-hello')
+
+config = ScriptRunConfig(source_directory='./tutorials/get-started-day1/code/hello', script='hello.py', compute_target='cpu-cluster')
+
+run = experiment.submit(config)
+RunDetails(run).show()
+
